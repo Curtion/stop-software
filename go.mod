1b6dd6b420ff51bba6c3ca5aa9a63afbf5a2fc92
Curtion <curtion@126.com>
2022-02-22 00:06:56 +0800

    使用json配置文件

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module stop-software
 
 go 1.17
+
+require github.com/antchfx/jsonquery v1.1.5
+
+require (
+	github.com/antchfx/xpath v1.2.0 // indirect
+	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
+)
--- main.go
+++ main.go
@@ -2,26 +2,32 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"os"
 	"os/exec"
-	"strings"
+
+	"github.com/antchfx/jsonquery"
 )
 
-func main() {
-	config, err := os.Open("./config.txt")
+func stop_software() {
+	config, err := os.Open("./config.json")
 	if err != nil {
 		fmt.Println(err)
 	}
 	defer config.Close()
-	byteValue, _ := ioutil.ReadAll(config)
-	arrayStr := strings.Split(string(byteValue), "\r\n")
-
-	for _, v := range arrayStr {
-		c := exec.Command("taskkill.exe", "/f", "/im", v)
+	doc, err := jsonquery.Parse(config)
+	if err != nil {
+		fmt.Println(err)
+	}
+	for _, n := range jsonquery.Find(doc, "software/*") {
+		fmt.Println(n.InnerText())
+		c := exec.Command("taskkill.exe", "/f", "/im", n.InnerText())
 		err = c.Start()
 		if err != nil {
 			fmt.Println(err)
 		}
 	}
+}
+
+func main() {
+
 }
