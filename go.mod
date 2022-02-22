f9d2c0dae76e7e8bc2ac873bfe9c3d081550d714
Curtion <curtion@126.com>
2022-02-22 10:57:24 +0800

    完成基本功能

--- go.mod
+++ go.mod
@@ -1,10 +1,13 @@
-module stop-software
+module stopSoftware
 
 go 1.17
 
 require github.com/antchfx/jsonquery v1.1.5
 
+require golang.org/x/sys v0.0.0-20220209214540-3681064d5158 // indirect
+
 require (
 	github.com/antchfx/xpath v1.2.0 // indirect
+	github.com/brunoqc/go-windows-session-notifications v0.0.0-20170424175830-fec440a22328
 	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
 )
--- main.go
+++ main.go
@@ -2,32 +2,74 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"os/exec"
+	"time"
 
 	"github.com/antchfx/jsonquery"
+	session_notifications "github.com/brunoqc/go-windows-session-notifications"
 )
 
-func stop_software() {
+func stop_software(doc *jsonquery.Node) {
+	log.Println("屏幕锁定，开始关闭程序")
+	for _, n := range jsonquery.Find(doc, "software/*") {
+		c := exec.Command("taskkill.exe", "/f", "/im", n.InnerText())
+		err := c.Start()
+		if err != nil {
+			log.Panicln(err)
+		}
+		log.Println(n.InnerText(), "关闭成功")
+	}
+}
+
+func check_time(doc *jsonquery.Node) bool {
+	timeValue := jsonquery.Find(doc, "time/*")
+	timeNowValue := fmt.Sprintf("%d:%d", time.Now().Hour(), time.Now().Minute())
+	return timeNowValue > timeValue[0].InnerText() && timeNowValue < timeValue[1].InnerText()
+}
+
+func main() {
 	config, err := os.Open("./config.json")
 	if err != nil {
-		fmt.Println(err)
+		log.Panicln(err)
 	}
 	defer config.Close()
 	doc, err := jsonquery.Parse(config)
 	if err != nil {
-		fmt.Println(err)
+		log.Panicln(err)
 	}
-	for _, n := range jsonquery.Find(doc, "software/*") {
-		fmt.Println(n.InnerText())
-		c := exec.Command("taskkill.exe", "/f", "/im", n.InnerText())
-		err = c.Start()
-		if err != nil {
-			fmt.Println(err)
+
+	quit := make(chan int)
+	chanMessages := make(chan session_notifications.Message, 100)
+	chanClose := make(chan int)
+
+	go func() {
+		for {
+			select {
+			case m := <-chanMessages:
+				switch m.UMsg {
+				case session_notifications.WM_WTSSESSION_CHANGE:
+					switch m.Param {
+					case session_notifications.WTS_SESSION_LOCK:
+						status := check_time(doc)
+						if !status {
+							stop_software(doc)
+						} else {
+							log.Println("屏幕锁定，但时间不满足")
+						}
+					case session_notifications.WTS_SESSION_UNLOCK:
+						log.Println("屏幕解锁")
+					}
+				case session_notifications.WM_QUERYENDSESSION:
+					log.Println("注销或关机")
+				}
+				close(m.ChanOk)
+			}
 		}
-	}
-}
+	}()
 
-func main() {
-
+	session_notifications.Subscribe(chanMessages, chanClose)
+	log.Println("程序已初始化")
+	<-quit
 }
